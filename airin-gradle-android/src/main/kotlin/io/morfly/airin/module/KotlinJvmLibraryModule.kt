74686dfc7b923d356f135dfaf989c00b97531cfc
Valera Zakharov <vzakharov@slack-corp.com>
2024-03-05 16:56:48 -0800

    Add KotlinJvmLibraryModule

---
+++ airin-gradle-android/src/main/kotlin/io/morfly/airin/module/KotlinJvmLibraryModule.kt
@@ -0,0 +1,47 @@
+package io.morfly.airin.module
+
+import include
+import io.morfly.airin.GradleModule
+import io.morfly.airin.ModuleComponent
+import io.morfly.airin.ModuleContext
+import io.morfly.airin.feature.ArtifactMappingFeature
+import io.morfly.pendant.starlark.glob
+import io.morfly.pendant.starlark.kt_jvm_library
+import io.morfly.pendant.starlark.lang.context.BUILD
+import io.morfly.pendant.starlark.lang.context.bazel
+import org.gradle.api.Project
+
+
+abstract class KotlinJvmLibraryModule : ModuleComponent() {
+
+    init {
+        include<ArtifactMappingFeature>()
+    }
+    override fun canProcess(project: Project): Boolean =
+        with(project.plugins) { hasPlugin("org.jetbrains.kotlin.jvm") }
+
+    override fun ModuleContext.onInvoke(module: GradleModule) {
+        val build =
+            BUILD.bazel {
+                _id = ID_BUILD
+
+                load("//bazel/macros:module.bzl", "kt_jvm_library")
+
+                kt_jvm_library {
+                    _id = ID_BUILD_TARGET_CALL
+
+                    name = module.name
+
+                    for ((config, deps) in module.dependencies) {
+                        config `=` deps.map { it.asBazelLabel().toString() }
+                    }
+                }
+            }
+        generate(build)
+    }
+
+    companion object {
+        const val ID_BUILD = "kt_jvm_library_build"
+        const val ID_BUILD_TARGET_CALL = "kt_jvm_target_call"
+    }
+}
