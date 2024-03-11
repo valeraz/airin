a5b377ace7587c9e42fc0866a47ba066f709e824
Valera Zakharov <vzakharov@slack-corp.com>
2024-03-11 15:30:32 -0700

    Revert "Remove resources glob from AndroidLibraryModule"
    This reverts commit 9fa03e99c3cb4af4b7a9cd402daa7f5322826379.

--- airin-gradle-android/src/main/kotlin/io/morfly/airin/module/AndroidLibraryModule.kt
+++ airin-gradle-android/src/main/kotlin/io/morfly/airin/module/AndroidLibraryModule.kt
@@ -49,6 +49,7 @@ abstract class AndroidLibraryModule : ModuleComponent() {
 
                 name = module.name
                 custom_package = module.androidMetadata?.packageName
+                resource_files = glob("src/main/res/**", allow_empty = True) // we have android libs without resources. Setting allow_empty as a workaround for now.
 
                 for ((config, deps) in module.dependencies) {
                     config `=` deps.map { it.asBazelLabel().toString() }.distinct()
